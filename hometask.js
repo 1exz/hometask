c516f461c940fd1ce1ade457e00f6a21ddcbdf7b
1exz <alex.buchko.m@gmail.com>
2022-04-07 00:01:48 +0300

    hometask2

--- hometask.js
+++ hometask.js
@@ -1,13 +1,17 @@
 let array=[]
     addArray()
        function addArray(){
-        let answers=prompt("Введите число: ");
-        if (!isNaN(parseFloat(answers)) ) {
+        let answers=+prompt("Введите число для массива : ");
+        if ( answers>0 ) {
             array.push(answers)
             addArray()
         }      
         else return array
     }
- 
-console.log(array)
+    let max = Math.max.apply(null,array);
+    let find=(array.indexOf(max))
+alert (`Самое больше число:${max} оно в массиве идет ${find+1} на  месте`)
+
+
+       
 
