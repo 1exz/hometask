d915550f56e433a952fd3333516401e091aa4e84
1exz <alex.buchko.m@gmail.com>
2022-04-09 09:56:09 +0300

    hometask5

--- hometask.js
+++ hometask.js
@@ -1,17 +1,48 @@
-let array=[]
-    addArray()
-       function addArray(){
-        let answers=+prompt("Введите число для массива : ");
-        if ( answers>0 ) {
-            array.push(answers)
-            addArray()
-        }      
-        else return array
+/*
+ Задача из 5 шагов-строк:
+ */
+let styles=["Джаз","Блюз"]
+console.log(styles)
+styles.push("РокэндРол")
+console.log(styles)
+console.log(styles.length)
+styles[styles.length-2]="Классика"
+console.log(styles)
+let elem=styles.shift()
+console.log(styles)
+console.log(`этот элемент ${elem}`)
+styles.unshift("Рэп","Рэгги")
+console.log(styles)
+ /*
+ 2) Написать функцию, которая возвращает последний элемент массивва 
+ */  
+function returnLastElem(arr){
+    return arr[arr.length-1]
+}
+console.log(returnLastElem([1,2,3,4,35]))
+/*
+ 3) Вычислить (суммы элементов массива):
+ */
+const x =[0.3,5.2, 9.4, 8]; 
+const y  =[43,5,3.4,8, 20];
+function summElem(x,y){
+    return (x.reduce((a,b)=>a+b,0)+y.reduce((a,b)=>a+b,0))/2
     }
-    let max = Math.max.apply(null,array);
-    let find=(array.indexOf(max))
-alert (`Самое больше число:${max} оно в массиве идет ${find+1} на  месте`)
+console.log(summElem(x,y))
 
 
-       
+/*
+4) Вычислить 
+C=(M!+N!)/(M+1)!
+M и N – числа введенные с клавиатуры.
+M! – это факториал числа, например, 5! = 1*2*3*4*5, это равно 120.
+Факториал должен вычисляться с помощью функции.
 
+ */
+let m=+prompt("введите число M")
+let n=+prompt("введите число N")
+function factorial(x) {
+    return (x != 1) ? x * factorial(x - 1) : 1;
+  }
+  c=((factorial(m)+factorial(n))/factorial(m+1))
+  console.log(c)
